af3ca5ba382bcb54c1335512f391ead1a4172e96
wurdum <pavel.ritikov@gmail.com>
2013-05-27 17:33:29 +0300

    load.html template was added

--- wsgi/run.py
+++ wsgi/run.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    return "This is the application cards & I'm alive"
+    return render_template('load.html')
 
 
 if __name__ == "__main__":
