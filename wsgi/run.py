68213411df3b0cefbf1f51bd48a4d75fb62dff3c
wurdum <pavel.ritikov@gmail.com>
2013-06-02 17:41:05 +0300

    cards table representation was completed

--- wsgi/run.py
+++ wsgi/run.py
@@ -32,18 +32,27 @@ def upload():
             if len([c for c in cards if not c.has_info or not c.has_prices]) > 0:
                 return redirect(url_for('stats', token=token))
 
-            return redirect(url_for('cards', token=token))
+            return redirect(url_for('cards', token=token, ltype='l'))
 
     return render_template('load.html', has_error=True)
 
 
-@app.route('/l/<token>', methods=['GET'])
-def cards(token=None):
+@app.route('/<ltype>/<token>', methods=['GET'])
+def cards(token=None, ltype='l'):
     cards = db.get_cards(token)
-    return render_template('cards_table.html', cards=cards, cards_num=sum([card.number for card in cards]),
-                           sum_prices={'low': models.calculate_sum(cards, 'low'),
-                                       'mid': models.calculate_sum(cards, 'mid'),
-                                       'high': models.calculate_sum(cards, 'high')})
+    templ_data = {'token': token, 'cards': cards, 'cards_num': sum([card.number for card in cards]),
+                  'sum_prices': {'low': models.calculate_sum(cards, 'low'),
+                                 'mid': models.calculate_sum(cards, 'mid'),
+                                 'high': models.calculate_sum(cards, 'high')}}
+
+    if ltype == 't':
+        templ_data['table'] = True
+        return render_template('cards_table.html', **templ_data)
+    elif ltype == 'l':
+        templ_data['list'] = True
+        return render_template('cards_list.html', **templ_data)
+    else:
+        return redirect(url_for('error'))
 
 
 @app.route('/s/<token>', methods=['GET'])
@@ -54,6 +63,7 @@ def stats(token=None):
 @app.route('/err')
 def error():
     return render_template('error.html')
+
 
 if __name__ == "__main__":
     app.run(debug="True")
